ac65bf3aec17326ab753d5a66dcbeb8d648e5fe3
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-03-03 15:26:07 -0300

    Finalizando o componente TÉCNICO-LIST

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -14,24 +14,23 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem('token');
 
-    if(token){
-      const cloneReq = request.clone({
-        headers: request.headers.set('Autorization', `Bearer ${token}`)
-      });
-      return next.handle(request);
-    }else{
+    if (token) {
+      const cloneReq = 
+        request.clone({ headers: request.headers.set('Authorization', `Bearer ${token}`) });
+        return next.handle(cloneReq);
+    } else {
       return next.handle(request);
     }
   }
 }
 
+
 export const AuthInterceptorProvider = [
   {
-  provider:HTTP_INTERCEPTORS,
-  useClass:AuthInterceptor,
-  multi:true
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
   }
 ]
-
